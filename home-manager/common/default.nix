239c31d390a94e9901a5691619c913b9a151f127
kidrigger <rex16saddler@gmail.com>
2024-04-27 09:31:15 +0200

    Added emacs to nix config.
    Emacs config is a submodule within the config to provide cross-compat.

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -1,4 +1,9 @@
-{ ... }: {
+{
+  self,
+  pkgs,
+  config,
+  ...
+}: {
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
@@ -10,4 +15,19 @@
     ./git.nix
     ./terminal
   ];
+
+  # Emacs
+  # TODO Add emacs nativecomp  
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-gtk;
+  };
+
+  home.file = {
+    "${config.home.homeDirectory}/.config/emacs/init.el".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/home-manager/common/emacs/init.el";
+    "${config.home.homeDirectory}/.config/emacs/early-init.el".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/home-manager/common/emacs/early-init.el";      
+    "${config.home.homeDirectory}/.config/emacs/config.org".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/home-manager/common/emacs/config.org";
+  };
+
+  # TODO Add emacs as a service.
 }
